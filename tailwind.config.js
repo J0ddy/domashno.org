0c8edd50b8a7ed8abf6088dab437811a25c7558e
J0ddy <me@joddy.dev>
2021-12-09 23:23:05 +0200

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,8 +13,16 @@ module.exports = {
       backgroundImage: {
         'hero': "url('/src/img/bg.jpeg')",
         'hero2': "url('/src/img/hero2.jpeg')",
-       }
-
+       },
+       stroke: {
+        current: 'currentColor',
+      },
+      stroke: theme => ({
+        'red': theme('colors.red.500'),
+        'green': theme('colors.green.500'),
+        'blue': theme('colors.blue.500'),
+        'black': theme('colors.black'),
+      })
     }
   },
   variants: {
